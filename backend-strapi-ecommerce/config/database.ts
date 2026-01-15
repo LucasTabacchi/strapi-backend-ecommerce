1e82878cc9bc688872bf7a65f69469e4a2e17695
Lucas <lucastabacchi31@gmail.com>
2026-01-15 18:44:16 -0300

    cambios

--- backend-strapi-ecommerce/config/database.ts
+++ backend-strapi-ecommerce/config/database.ts
@@ -1,20 +1,18 @@
 export default ({ env }) => ({
   connection: {
-    client: 'postgres',
+    client: "postgres",
     connection: {
-      host: env('PGHOST'),
-      port: env.int('PGPORT', 5432),
-      database: env('PGDATABASE'),
-      user: env('PGUSER'),
-      password: env('PGPASSWORD'),
-      ssl: env.bool('DATABASE_SSL', true)
-        ? { rejectUnauthorized: false }
-        : false,
+      host: env("PGHOST"),
+      port: env.int("PGPORT", 5432),
+      database: env("PGDATABASE"),
+      user: env("PGUSER"),
+      password: env("PGPASSWORD"),
+      ssl: env.bool("DATABASE_SSL", true) ? { rejectUnauthorized: false } : false,
     },
     pool: {
-      min: 0,
-      max: 10,
+      min: env.int("DATABASE_POOL_MIN", 0),
+      max: env.int("DATABASE_POOL_MAX", 2),
     },
-    acquireConnectionTimeout: 60000,
+    acquireConnectionTimeout: env.int("DATABASE_POOL_ACQUIRE_TIMEOUT", 120000),
   },
 });
