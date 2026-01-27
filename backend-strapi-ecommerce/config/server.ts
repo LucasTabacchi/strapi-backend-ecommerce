4b20737ca2d6fb692b67a9ebfabf0e28a086bdb9
Lucas <lucastabacchi31@gmail.com>
2026-01-27 04:40:05 -0300

    cambios en server.ts y index.ts

--- backend-strapi-ecommerce/config/server.ts
+++ backend-strapi-ecommerce/config/server.ts
@@ -1,7 +1,14 @@
-module.exports = ({ env }) => ({
-  host: env("HOST", "0.0.0.0"),
-  port: env.int("PORT", 1337),
-  url: env("PUBLIC_URL"), // URL pública de Railway
-  proxy: true,
-  app: { keys: env.array("APP_KEYS") },
-});
+module.exports = ({ env }) => {
+  const publicUrl =
+    env("PUBLIC_URL") ||
+    env("RENDER_EXTERNAL_URL") || // Render suele exponer esta variable
+    "http://localhost:1337";
+
+  return {
+    host: env("HOST", "0.0.0.0"),
+    port: env.int("PORT", 1337),
+    url: publicUrl,
+    proxy: true,
+    app: { keys: env.array("APP_KEYS") },
+  };
+};
